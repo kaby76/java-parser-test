20f322aec39018eba43f33973a09a9b172699c84
Ken Domino <kenneth.domino@domemtech.com>
2019-11-07 17:09:11 -0500

    Update with more to test and times.

--- build.sh
+++ build.sh
@@ -6,6 +6,33 @@ cp ../grammars-v4/java/Java*.g4 .
 javac -cp 'C:\Program Files\Java\javalib\antlr-4.7.2-complete.jar' *.java
 "$JAVA_HOME/bin/java.exe" -cp ".;$Antlr4ToolPath" FunctionValidate
 
+date
+
+files=`find ../android-sdk-sources-for-api-level-5 -name '*.java'`
+for i in $files
+do
+	lines=`wc $i | awk '{print $1}'`
+	start=`date +%s.%N`
+	data=`"$JAVA_HOME/bin/java.exe" -cp ".;$Antlr4ToolPath" FunctionValidate $i`
+	end=`date +%s.%N`
+	runtime=`awk "BEGIN {print $end - $start }"`
+	echo $runtime $lines $data $i
+done
+
+date
+
+files=`find ../android-sdk-sources-for-api-level-29 -name '*.java'`
+for i in $files
+do
+	lines=`wc $i | awk '{print $1}'`
+	start=`date +%s.%N`
+	data=`"$JAVA_HOME/bin/java.exe" -cp ".;$Antlr4ToolPath" FunctionValidate $i`
+	end=`date +%s.%N`
+	runtime=`awk "BEGIN {print $end - $start }"`
+	echo $runtime $lines $data $i
+done
+
+date
 
 files=`find ../jdk/src -name '*.java'`
 for i in $files
@@ -18,13 +45,4 @@ do
 	echo $runtime $lines $data $i
 done
 
-files=`find ../../android-sdk-sources-for-api-level-5 -name '*.java'`
-for i in $files
-do
-	lines=`wc $i | awk '{print $1}'`
-	start=`date +%s.%N`
-	"$JAVA_HOME/bin/java.exe" -cp ".;$Antlr4ToolPath" FunctionValidate $i
-	end=`date +%s.%N`
-	runtime=`awk "BEGIN {print $end - $start }"`
-	echo $runtime $lines $i
-done
+date
