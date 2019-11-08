39890f0cb870429587989241511a729f24f7739a
Ken Domino <kenneth.domino@domemtech.com>
2019-11-08 03:06:53 -0500

    Tests now in different script.

--- build.sh
+++ build.sh
@@ -11,50 +11,11 @@ cd ..
 
 date
 
-cd java-parser
+cd csharp-parser
 cp ../grammars-v4/java/Java*.g4 .
 dotnet restore
 dotnet build
 dotnet run
 cd ..
-
-date
-
-files=`find ../android-sdk-sources-for-api-level-5 -name '*.java'`
-for i in $files
-do
-	lines=`wc $i | awk '{print $1}'`
-	start=`date +%s.%N`
-	data=`"$JAVA_HOME/bin/java.exe" -cp ".;$Antlr4ToolPath" FunctionValidate $i`
-	end=`date +%s.%N`
-	runtime=`awk "BEGIN {print $end - $start }"`
-	echo $runtime $lines $data $i
-done
-
-date
-
-files=`find ../android-sdk-sources-for-api-level-29 -name '*.java'`
-for i in $files
-do
-	lines=`wc $i | awk '{print $1}'`
-	start=`date +%s.%N`
-	data=`"$JAVA_HOME/bin/java.exe" -cp ".;$Antlr4ToolPath" FunctionValidate $i`
-	end=`date +%s.%N`
-	runtime=`awk "BEGIN {print $end - $start }"`
-	echo $runtime $lines $data $i
-done
-
-date
-
-files=`find ../jdk/src -name '*.java'`
-for i in $files
-do
-	lines=`wc $i | awk '{print $1}'`
-	start=`date +%s.%N`
-	data=`"$JAVA_HOME/bin/java.exe" -cp ".;$Antlr4ToolPath" FunctionValidate $i`
-	end=`date +%s.%N`
-	runtime=`awk "BEGIN {print $end - $start }"`
-	echo $runtime $lines $data $i
-done
 
 date
